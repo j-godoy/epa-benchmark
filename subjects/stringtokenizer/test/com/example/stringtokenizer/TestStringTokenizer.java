0cd87416d1664943faa7f7822a79916719beb640
JGodoy <j.godoy277@gmail.com>
2018-11-05 18:36:59 -0300

    add stringtokenizer subject

---
+++ subjects/stringtokenizer/test/com/example/stringtokenizer/TestStringTokenizer.java
@@ -0,0 +1,14 @@
+package com.example.stringtokenizer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStringTokenizer {
+
+	@Test
+	public void test01() {
+		StringTokenizer t = new StringTokenizer("");
+	}
+
+}
