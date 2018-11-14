f74d05f74fe44171bdaedf89662ce84d091a6044
JGodoy <j.godoy277@gmail.com>
2018-11-14 16:02:35 -0300

    rename smtp folder

---
+++ subjects/smtpprocessor_h/instrumented/com/example/smtp/MockInterruptedIOException.java
@@ -0,0 +1,10 @@
+package com.example.smtp;
+
+public class MockInterruptedIOException extends MockIOException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ subjects/smtpprocessor_h/instrumented/com/example/smtp/VerifyIP.java
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * This program is a 100% Java Email Server.
+ ******************************************************************************
+ * Copyright (c) 2001-2009, Eric Daugherty (http://www.ericdaugherty.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ * For current versions and more information, please visit:
+ * http://www.ericdaugherty.com/java/mail
+ *
+ * or contact the authors at:
+ * java@ericdaugherty.com
+ * andreask1@vivodinet.gr
+ *
+ ******************************************************************************
+ * This program is based on the CSRMail project written by Calvin Smith.
+ * http://crsemail.sourceforge.net/
+ ******************************************************************************
+ *
+ * $Rev$
+ * $Date$
+ *
+ ******************************************************************************/
+
+package com.example.smtp;
+
+/**
+ * The interface for verifyIP process classes
+ *
+ * @author Andreas Kyrmegalos
+ */
+public interface VerifyIP {
+
+   public boolean blockIP(String clientIP);
+
+   public void saveBegin(MockSMTPMessage message, boolean useAmavisSMTPDirectory) throws MockIOException;
+}
