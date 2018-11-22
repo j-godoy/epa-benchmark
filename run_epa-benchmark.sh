fa8c2b400dcf07a4ccc90299f917b33890dd695c
j-godoy <j.godoy277@gmail.com>
2018-11-22 13:11:39 -0300

    edit run_epa-benchmark.sh

--- run_epa-benchmark.sh
+++ run_epa-benchmark.sh
@@ -1,11 +1,11 @@
 #### epatesting Start.sh ###
 #!/bin/bash
 
-echo "Iniciando Epa Testing..."
+echo "Iniciando Epa Benchmark..."
 export _JAVA_OPTIONS=-Xmx1024m
 cd /home/jgodoy/workspace-epa/epatesting
 newName=$(date +%Y-%m-%d_%H%M%S)
 newName=epa_output.$newName.txt
-rm -r /home/jgodoy/workspace-epa/epatesting/evosuite-report/ /home/jgodoy/workspace-epa/epatesting/all_resumes.csv
-nohup python3.6 /home/jgodoy/workspace-epa/epatesting/script.py "/home/jgodoy/workspace-epa/epatesting/config_example_local.ini" "/home/jgodoy/workspace-epa/epatesting/runs_example_local.ini" > /home/jgodoy/workspace-epa/epatesting/$newName &
+rm -r /home/jgodoy/Replication-Package/epa-benchmark/evosuite-report/ /home/jgodoy/Replication-Package/epa-benchmark/results/all_resumes.csv
+nohup python3.6 /home/jgodoy/Replication-Package/epa-benchmark/script.py "/home/jgodoy/Replication-Package/epa-benchmark/config_example.ini" "/home/jgodoy/Replication-Package/epa-benchmark/runs_example.ini" > /home/jgodoy/Replication-Package/epa-benchmark/results/$newName &
 exit 0
