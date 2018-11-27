6b9352cdce0fbbbd393fa4682f3b06dc89f24b60
JGodoy <j.godoy277@gmail.com>
2018-11-27 16:15:39 -0300

    add r_script

---
+++ r_scripts/mean_pit.R
@@ -0,0 +1,103 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)!=1) {
+  stop("Input csv file argument must be supplied (report.csv)", call.=FALSE)
+}
+
+csv_filename = args[1]
+
+
+# run the script
+stats = read.csv(csv_filename, header=TRUE, sep=",")
+
+subjects = unique(stats$SUBJ)
+tools = unique(stats$TOOL)
+budgets = unique(stats$BUD)
+
+printPitMutationScoreMedian <- function() {
+	for(subj in subjects) {
+		for (budget in budgets) {
+			if (budget=='600') {
+				cat("###############################################################\n")
+				cat("subject:", subj)
+				cat("\n")
+				cat("budget:",budget)
+				cat("\n")
+				cat("###############################################################\n")
+
+				
+			# LINE:BRANCH
+			default_rows  = subset(stats,SUBJ==subj & TOOL=='evosuite_default' & BUD==budget)
+			line_branch_errors = default_rows$PIMUT
+			cat("length(LINE:BRANCH)=", length(line_branch_errors))
+			cat("\n")
+			cat("mean(LINE:BRANCH)=", mean(line_branch_errors))
+			cat("\n")
+			
+			# LINE:BRANCH:EXCEPTION
+			default_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception' & BUD==budget)
+			default_errors = default_rows$PIMUT
+			cat("length(LINE:BRANCH:EXCEPTION)=", length(default_errors))
+			cat("\n")
+			cat("mean(LINE:BRANCH:EXCEPTION)=", mean(default_errors))
+			cat("\n")
+			
+			# EPATRANSITION
+			epatransition_rows  = subset(stats,SUBJ==subj & TOOL=='epatransition' & BUD==budget)
+			epatransition_errors = epatransition_rows$PIMUT
+			cat("length(EPATRANSITION)=", length(epatransition_errors))
+			cat("\n")
+			cat("mean(EPATRANSITION)=", mean(epatransition_errors))
+			cat("\n")
+			
+			# EPATRANSITION:EPAEXCEPTION
+			epatransition_epaexception_rows = subset(stats,SUBJ==subj & TOOL=='epatransition_epaexception' & BUD==budget)
+			epatransition_epaexception_errors = epatransition_epaexception_rows$PIMUT
+			cat("length(EPATRANSITION:EPAEXCEPTION)=", length(epatransition_epaexception_errors))
+			cat("\n")
+			cat("mean(EPATRANSITION:EPAEXCEPTION)=", mean(epatransition_epaexception_errors))
+			cat("\n")
+			
+			# EPAADJACENTEDGES
+			edges_rows = subset(stats,SUBJ==subj & TOOL=='epaadjacentedges' & BUD==budget)
+			edges_errors = edges_rows$PIMUT
+			cat("length(EPAADJACENTEDGES)=", length(edges_errors))
+			cat("\n")
+			cat("mean(EPAADJACENTEDGES)=", mean(edges_errors))
+			cat("\n")
+
+			# LINE:BRANCH:EXCEPTION:EPATRANSITION
+			epamixed_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception_epatransition' & BUD==budget)
+			line_branch_exception_epatransition_errors = epamixed_rows$PIMUT
+			cat("length(LINE:BRANCH:EXCEPTION:EPATRANSITION)=", length(line_branch_exception_epatransition_errors))
+			cat("\n")
+			cat("mean(LINE:BRANCH:EXCEPTION:EPATRANSITION)=", mean(line_branch_exception_epatransition_errors))
+			cat("\n")
+
+			# LINE:BRANCH:EXCEPTION:EPATRANSITION:EPAEXCEPTION
+			line_branch_exception_epatransition_epaexception_rows = subset(stats,SUBJ==subj & TOOL=='line_branch_exception_epatransition_epaexception' & BUD==budget)
+			line_branch_exception_epatransition_epaexception_errors = line_branch_exception_epatransition_epaexception_rows$PIMUT
+			cat("length(LINE:BRANCH:EXCEPTION:EPATRANSITION:EPAEXCEPTION)=", length(line_branch_exception_epatransition_epaexception_errors))
+			cat("\n")
+			cat("mean(LINE:BRANCH:EXCEPTION:EPATRANSITION:EPAEXCEPTION)=", mean(line_branch_exception_epatransition_epaexception_errors))
+			cat("\n")
+
+			# LINE:BRANCH:EXCEPTION:EPAADJACENTEDGES
+			line_branch_exception_edges_rows = subset(stats,SUBJ==subj & TOOL=='line_branch_exception_epaadjacentedges' & BUD==budget)
+			line_branch_exception_edges_errors = line_branch_exception_edges_rows$PIMUT
+			cat("length(LINE:BRANCH:EXCEPTION:EPAADJACENTEDGES)=", length(line_branch_exception_edges_errors))
+			cat("\n")
+			cat("mean(LINE:BRANCH:EXCEPTION:EPAADJACENTEDGES)=", mean(line_branch_exception_edges_errors))
+			cat("\n")
+			cat("\n")
+
+			}
+		}
+	}
+}
+
+printPitMutationScoreMedian()
+
+
