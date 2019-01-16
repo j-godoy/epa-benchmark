3a34cbc983566471600a2908de46a7eff13901e0
JGodoy <j.godoy277@gmail.com>
2019-01-16 17:21:55 -0300

    comparo en boxplots 6 criterios

--- r_scripts/boxplot/boxplot_pit.R
+++ r_scripts/boxplot/boxplot_pit.R
@@ -49,34 +49,38 @@ createBoxPlot <- function()
 			{
 				default_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception' & BUD==budget)
 				line_branch_exception_epatransition_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception_epatransition' & BUD==budget)
-				line_branch_exception_epatransition_epaexception_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception_epatransition_epaexception' & BUD==budget)
 				line_branch_exception_edges_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception_epaadjacentedges' & BUD==budget)
+				line_branch_exception_smc_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception_strongmutation' & BUD==budget)
+				line_branch_exception_mosa_rows  = subset(stats,SUBJ==subj & TOOL=='mosa_line_branch_exception_strongmutation' & BUD==budget)
+				randoop_rows  = subset(stats,SUBJ==subj & TOOL=='randoop' & BUD==budget)
 
 				default_pit = default_rows$PIMUT
 				line_branch_exception_epatransition_pit = line_branch_exception_epatransition_rows$PIMUT
-				line_branch_exception_epatransition_epaexception_pit = line_branch_exception_epatransition_epaexception_rows$PIMUT
 				line_branch_exception_edges_pit = line_branch_exception_edges_rows$PIMUT
+				line_branch_exception_smc_pit = line_branch_exception_smc_rows$PIMUT
+				line_branch_exception_mosa_pit = line_branch_exception_mosa_rows$PIMUT
+				line_branch_exception_randoop_pit = randoop_rows$PIMUT
 
 				name_subj = strsplit(subj, "[.]")[[1]]
 				name_subj = tail(name_subj, n=1)
-				outputname = paste("def_plus_epa_criterion_",name_subj,".pdf",sep = "")
+				outputname = paste("boxplot_combined_",name_subj,".pdf",sep = "")
 				pdf(outputname)
 				budget_txt = paste("Budget: ", budget, " segs", sep="")
-				boxplot(default_pit, line_branch_exception_epatransition_pit, line_branch_exception_epatransition_epaexception_pit, line_branch_exception_edges_pit, names=c("Base","Base+Epa","Base+Epax","Base+Epaxp"),main=c(name_subj),ylab="PIT Score",xlab=budget_txt,cex.lab=1.5, cex.axis=1.2)
+				boxplot(default_pit, line_branch_exception_epatransition_pit, line_branch_exception_edges_pit, line_branch_exception_smc_pit, line_branch_exception_mosa_pit, line_branch_exception_randoop_pit, names=c("A","B","C","D","E","F"),main=c(name_subj),ylab="Mutation Score",cex.lab=1.6, cex.axis=1.3, cex.main=2)
 				
-				default_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception' & BUD==budget)
-				epatransition_rows  = subset(stats,SUBJ==subj & TOOL=='epatransition' & BUD==budget)
-				epatransition_epaexception_rows  = subset(stats,SUBJ==subj & TOOL=='epatransition_epaexception' & BUD==budget)
-				edges_rows  = subset(stats,SUBJ==subj & TOOL=='epaadjacentedges' & BUD==budget)
+				#default_rows  = subset(stats,SUBJ==subj & TOOL=='line_branch_exception' & BUD==budget)
+				#epatransition_rows  = subset(stats,SUBJ==subj & TOOL=='epatransition' & BUD==budget)
+				#epatransition_epaexception_rows  = subset(stats,SUBJ==subj & TOOL=='epatransition_epaexception' & BUD==budget)
+				#edges_rows  = subset(stats,SUBJ==subj & TOOL=='epaadjacentedges' & BUD==budget)
 
-				default_pit = default_rows$PIMUT
-				epatransition_pit = epatransition_rows$PIMUT
-				epatransition_epaexception_pit = epatransition_epaexception_rows$PIMUT
-				edges_pit = edges_rows$PIMUT
+				#default_pit = default_rows$PIMUT
+				#epatransition_pit = epatransition_rows$PIMUT
+				#epatransition_epaexception_pit = epatransition_epaexception_rows$PIMUT
+				#edges_pit = edges_rows$PIMUT
 				
-				outputname = paste("only_epa_criterion_",name_subj,".pdf",sep = "")
-				pdf(outputname)
-				boxplot(default_pit, epatransition_pit, epatransition_epaexception_pit, edges_pit, names=c("Base","Epa","Epax","Epaxp"),main=c(name_subj),ylab="PIT Score",xlab=budget_txt,cex.lab=1.5, cex.axis=1.6)
+				#outputname = paste("only_epa_criterion_",name_subj,".pdf",sep = "")
+				#pdf(outputname)
+				#boxplot(default_pit, epatransition_pit, epatransition_epaexception_pit, edges_pit, names=c("EVO","EVO+EPA","EVO+EPAXP","EVO+SMC","EVOMOSA","RAND"),main=c(name_subj),ylab="PIT Score",xlab=budget_txt,cex.lab=1.5, cex.axis=1.6)
 			}
 		}
 	}
