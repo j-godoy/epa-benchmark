4ff3816d151d4620b29ad660d5d0f1aed6e4e763
JGodoy <j.godoy277@gmail.com>
2019-10-24 13:00:28 -0300

    minor

--- run_epa-benchmark.sh
+++ run_epa-benchmark.sh
@@ -6,6 +6,6 @@ export _JAVA_OPTIONS=-Xmx1024m
 cd /home/jgodoy/workspace-epa/epatesting
 newName=$(date +%Y-%m-%d_%H%M%S)
 newName=epa_output.$newName.txt
-rm -r /home/jgodoy/Replication-Package/epa-benchmark/evosuite-report/ /home/jgodoy/Replication-Package/epa-benchmark/results/all_resumes.csv
+rm -r /home/jgodoy/Replication-Package/epa-benchmark/evosuite-report/
 nohup python3.6 /home/jgodoy/Replication-Package/epa-benchmark/script.py "/home/jgodoy/Replication-Package/epa-benchmark/config_example.ini" "/home/jgodoy/Replication-Package/epa-benchmark/runs_example.ini" > /home/jgodoy/Replication-Package/epa-benchmark/results/$newName &
 exit 0
