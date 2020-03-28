b76d5dacba702701cced0d5b7fef3a9ebd2a9d06
jgodoy <j.godoy277@gmail.com>
2020-03-27 23:24:56 -0300

    add subject listiterator

---
+++ subjects_newpre/ListIterator/mining/com/example/listitr/remove/NoSuchElementException.java
@@ -0,0 +1,48 @@
+/*
+ * @(#)NoSuchElementException.java	1.16 98/03/18
+ *
+ * Copyright 1994-1998 by Sun Microsystems, Inc.,
+ * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Sun Microsystems, Inc. ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Sun.
+ */
+
+package com.example.listitr.remove;
+
+/**
+ * Thrown by the <code>nextElement</code> method of an 
+ * <code>Enumeration</code> to indicate that there are no more 
+ * elements in the enumeration. 
+ *
+ * @author  unascribed
+ * @version 1.16, 03/18/98
+ * @see     java.util.Enumeration
+ * @see     java.util.Enumeration#nextElement()
+ * @since   JDK1.0
+ */
+public
+class NoSuchElementException extends RuntimeException {
+    /**
+     * Constructs a <code>NoSuchElementException</code> with <tt>null</tt> 
+     * as its error message string.
+     */
+    public NoSuchElementException() {
+	super();
+    }
+
+    /**
+     * Constructs a <code>NoSuchElementException</code>, saving a reference 
+     * to the error message string <tt>s</tt> for later retrieval by the 
+     * <tt>getMessage</tt> method.
+     *
+     * @param   s   the detail message.
+     */
+    public NoSuchElementException(String s) {
+	super(s);
+    }
+}
