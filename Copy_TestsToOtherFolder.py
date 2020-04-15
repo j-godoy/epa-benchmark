1452cddedb2fbdd186e9d9f5c4449df76ab55a36
jgodoy <j.godoy277@gmail.com>
2020-04-15 17:03:49 -0300

    edit copytests file

--- Copy_TestsToOtherFolder.py
+++ Copy_TestsToOtherFolder.py
@@ -4,7 +4,7 @@ import utils
 import re
 
 
-def copy_test(inputs, output, id, n, qualified_name, keywords_to_remove_file, replace_in_imports):
+def copy_test(inputs, output, id, n, qualified_name, keywords_to_remove_file, replace_in_imports, replace_original_testsuite):
     def get_rename_test(java_file, class_name, new_test_file_name, replace_in_imports):
         replace_import = "=" in replace_in_imports
         old = ""
@@ -114,6 +114,11 @@ def copy_test(inputs, output, id, n, qualified_name, keywords_to_remove_file, re
             new_test_file = get_rename_test(test_file, class_name, new_test_file_name_without_extension, replace_in_imports)
             new_test_file = remove_flacky_test(new_test_file_name_without_extension, new_test_file, keywords_to_remove_file)
             utils.save_file(test_file_output, new_test_file)
+
+            ###Replace_original TS
+            if replace_original_testsuite == "True":
+                new_test_file = get_rename_test(test_file_output, new_test_file_name_without_extension.replace("_ESTest",""), class_name+"_ESTest", replace_in_imports)
+                utils.save_file(test_file, new_test_file)
             i += 1
             test_file = os.path.join(input, str(i), "test", package_dir, file_name)
     print("saved files to "+ output + "...")
@@ -130,6 +135,9 @@ if __name__ == '__main__':
     parser.add_argument("qualified_name_class", help="fully qualified name for subject to copy test suite")
     parser.add_argument("keywords_to_remove_file", help="keywords_to_remove in test suite")
     parser.add_argument("replace_in_imports", help="replace in imports of test suite. Example: package name")
+    parser.add_argument("replace_original_testsuite", help="replace original test suite (removing test cases). True or False")
     args = parser.parse_args()
-    copy_test(args.source_path, args.dest_path, args.id, int(args.how_many_testsuite), args.qualified_name_class, args.keywords_to_remove_file,  args.replace_in_imports)
+    if args.replace_original_testsuite == "True":
+        print("Replace original test suite, too!")
+    copy_test(args.source_path, args.dest_path, args.id, int(args.how_many_testsuite), args.qualified_name_class, args.keywords_to_remove_file,  args.replace_in_imports, args.replace_original_testsuite)
     print("Done!")
